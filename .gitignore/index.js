ea4a7e084d0c96870c407059f62dbf6e75f35a5e
Shihoru <48069034+Shihoru@users.noreply.github.com>
2019-02-27 21:07:41 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const Tableau = require('./commandes/Tableau')
+const bot = new Discord.Client();
+
+
+bot.login(process.env.TOKEN);
+
+bot.on('ready', function() {
+	bot.user.setActivity('Preparation du systeme gvg').catch(console.error)
+});
+
+
+bot.on('message', function (message) {
+
+if (Tableau.match(message)){
+	return Tableau.action(message)
+}
+
+if(message.content == "Salut"){
+	message.reply("Hello :raised_hands:");
+	console.log("Le bot dit bonjour");
+}
+});
+
